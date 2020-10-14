8da62b662f804ca34ec32f4300f8e50232aff9f8
Skander <skander@bibani.nl>
2020-10-14 16:39:52 +0200

    Add reminders to list and notifies adapter

--- app/src/main/java/com/example/madlevel2example/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2example/MainActivity.kt
@@ -2,10 +2,56 @@ package com.example.madlevel2example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.madlevel2example.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val reminders = arrayListOf<Reminder>()
+    // Items into the adapter and get used to create a view
+    private val reminderAdapter = ReminderAdapter(reminders)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.btnAddReminder.setOnClickListener {
+            val reminder = binding.etReminder.text.toString()
+            // Add text value to the list
+            addReminder(reminder)
+        }
+
+        // Adds LinearLayoutManager to recyclerview
+        // Keep in mind Views are hierarchical
+        binding.rvReminders.layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.VERTICAL, false)
+        binding.rvReminders.adapter = reminderAdapter
+
+        // ?
+        binding.rvReminders.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+    }
+
+    private fun addReminder(reminder: String) {
+        // When its not empty
+        if (reminder.isNotBlank()) {
+            // Add a reminder item.
+            reminders.add(Reminder(reminder))
+            // Notify Adapter that the data model has changed so it has to update its view.
+            reminderAdapter.notifyDataSetChanged()
+            // Reset the input text.
+            binding.etReminder.text?.clear()
+        } else {
+            Snackbar.make(etReminder, "You must fill in the input field!", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
